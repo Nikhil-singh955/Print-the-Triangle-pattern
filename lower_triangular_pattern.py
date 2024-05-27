86d57558966dc2f91802aabf158d03d738cca071
Nikhil Singh <nikhil1509singh@gmail.com>
2024-05-27 11:30:59 +0530

    initial commit

---
+++ lower_triangular_pattern.py
@@ -0,0 +1,6 @@
+def lower_triangular(n):
+    for i in range(1, n + 1):
+        print('*' * i)
+
+n = 5
+lower_triangular(n)
---
+++ upper_triangular_pattern.py
@@ -0,0 +1,6 @@
+def upper_triangular(n):
+    for i in range(n, 0, -1):
+        print('*' * i)
+
+n = 5
+upper_triangular(n)
